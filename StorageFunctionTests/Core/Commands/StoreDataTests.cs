7b47465e0c616bce0ad5acf1502588d0ccfff16f
Your Name <rsmith5901@gmail.com>
2019-06-02 11:34:06 -0400

    Writin some tests

---
+++ StorageFunctionTests/Core/Commands/StoreDataTests.cs
@@ -0,0 +1,38 @@
+﻿namespace StorageFunctionTests.Core.Commands
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using StorageFunction.Core.Commands;
+    using StorageFunction.Core.Entities;
+    using StorageFunction.Core.Interfaces;
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class StoreDataTests
+    {
+        [TestMethod]
+        public async Task Handle_CallsStoreWithRequest_ReturnsResult()
+        {
+            var mockProcessor = new Mock<IStorageProcessor>();
+
+            var sut = new StoreDataHandler(mockProcessor.Object);
+        
+            var request = new StorageRequest
+            {
+                Base64Data = "basedata",
+                ContainerName = "containername",
+                ContentType = "application/json"
+            };
+
+            var response = await sut.Handle(request, It.IsAny<CancellationToken>());
+
+            Func<StorageRequest, bool> matches = r => r.Base64Data == request.Base64Data &&
+                                                      r.ContentType == request.ContentType &&
+                                                      r.ContainerName == request.ContainerName;
+
+            mockProcessor.Verify(p => p.Store(It.Is<StorageRequest>(r => matches(r))));
+        }
+    }
+}
---
+++ StorageFunctionTests/Core/Entities/StorageRequestTests.cs
@@ -0,0 +1,76 @@
+﻿namespace StorageFunctionTests.Core.Entities
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using StorageFunction.Core.Entities;
+
+    [TestClass]
+    public class StorageRequestTests
+    {
+        [TestMethod]
+        public void GivenAnEmailRequest_WhenValid_ReturnsIsValidTrue()
+        {
+            string base64Decoded = "base64 encoded string";
+            string base64Encoded;
+            byte[] data = System.Text.ASCIIEncoding.ASCII.GetBytes(base64Decoded);
+            base64Encoded = System.Convert.ToBase64String(data);
+
+            var sut = new StorageRequest
+            {
+                Base64Data = base64Encoded,
+                ContentType = "Application/Json",
+                ContainerName = "Container Name"
+            };
+
+            Assert.IsTrue(sut.IsValid());
+        }
+
+        [TestMethod]
+        public void GivenAnEmailRequest_WhenNotValidBase64Data_ReturnsIsValidFalse()
+        {
+            var sut = new StorageRequest
+            {
+                Base64Data = "-- InValid Base 64 Data --",
+                ContentType = "Application/Json",
+                ContainerName = "Container Name"
+            };
+
+            Assert.IsFalse(sut.IsValid());
+        }
+
+        [TestMethod]
+        public void GivenAnEmailRequest_WhenEmptyContentType_ReturnsIsValidFalse()
+        {
+            string base64Decoded = "base64 encoded string";
+            string base64Encoded;
+            byte[] data = System.Text.ASCIIEncoding.ASCII.GetBytes(base64Decoded);
+            base64Encoded = System.Convert.ToBase64String(data);
+
+            var sut = new StorageRequest
+            {
+                Base64Data = base64Encoded,
+                ContentType = "",
+                ContainerName = "Container Name"
+            };
+
+            Assert.IsFalse(sut.IsValid());
+        }
+
+        [TestMethod]
+        public void GivenAnEmailRequest_WhenEmptyContainerName_ReturnsIsValidFalse()
+        {
+            string base64Decoded = "base64 encoded string";
+            string base64Encoded;
+            byte[] data = System.Text.ASCIIEncoding.ASCII.GetBytes(base64Decoded);
+            base64Encoded = System.Convert.ToBase64String(data);
+
+            var sut = new StorageRequest
+            {
+                Base64Data = base64Encoded,
+                ContentType = "Application/Json",
+                ContainerName = ""
+            };
+
+            Assert.IsFalse(sut.IsValid());
+        }
+    }
+}
